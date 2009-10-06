da8a419798a07514031624d8ecf3a897b0aac1ae
all_that_jazz26@hotmail.com <all_that_jazz26@hotmail.com@44053aa2-a284-11de-bfb1-cf57f9aef694>
2009-10-06 01:34:20 +0000

    Floor added to heli

--- trunk/Assignment 2/main.cpp
+++ trunk/Assignment 2/main.cpp
@@ -325,6 +325,14 @@ void drawHeliBody()
 	glVertex3f(3.0, 0.0, 0.0);
 	glEnd();
 
+	//Floor
+	glBegin(GL_POLYGON);
+	glVertex3f(0.0, 0.0, 0.0);
+	glVertex3f(3.0, 0.0, 0.0);
+	glVertex3f(3.0, 0.0, -1.0);
+	glVertex3f(0.0, 0.0, -1.0);
+	glEnd();
+
 	glPopMatrix();
 
 	//Tail
